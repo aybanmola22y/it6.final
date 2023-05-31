92bce6f7586efdac7d9e86c7e925d0df2810cf18
aybanxxx <molatojohnaivanneofficial@gmail.com>
2023-06-01 00:13:56 +0800

    add update feature, format with black

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -11,23 +11,78 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/customers", methods=["GET"])
-def get_customers():
+
+def data_fetch(query):
     cur = mysql.connection.cursor()
-    query="""
-    select * from customer
-    """
     cur.execute(query)
     data = cur.fetchall()
-    cur.close()
-    
+    return data
+
+
+@app.route("/customers", methods=["GET"])
+def get_customers():
+    query = """
+    select * from customer
+    """
+    data = data_fetch("""select * from customer""")
     return make_response(jsonify(data), 200)
 
-    
+
+@app.route("/customers/<int:id>", methods=["GET"])
+def get_customer_by_id(id):
+    data = data_fetch("""select * from customer where customer_id = {}""".format(id))
+    return make_response(jsonify(data), 200)
+
+
+@app.route("/customers", methods=["POST"])
+def add_actors():
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    firstName = info["firstName"]
+    lastName = info["lastName"]
+    cur.execute(
+        """ insert into customer (firstName, lastName) value (%s, %s)""",
+        (firstName, lastName),
+    )
+
+    mysql.connection.commit()
+    print("row(s) affected :{}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    cur.close()
+    return make_response(
+        jsonify(
+            {"message": "customer added successfully", "rows_affected": rows_affected}
+        ),
+        201,
+    )
+
+
+@app.route("/customers/<int:id>", methods=["PUT"])
+def update_actor(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    firstName = info["firstName"]
+    lastName = info["lastName"]
+    cur.execute(
+        """
+        update customer set firstName = %s, lastName = %s where customer_id = %s
+    """,
+        (firstName, lastName, id),
+    )
+    mysql.connection.commit()
+    rows_affected = cur.rowcount
+    cur.close()
+    return make_response(
+        jsonify(
+            {"message": "customer updated successfully", "rows_affected": rows_affected}
+        ),
+        201,
+    )
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
-import unittest 
+import unittest
 import warnings
 from api import app
+
 
 class MyAppTests(unittest.TestCase):
     def setUp(self):
@@ -14,15 +15,16 @@ class MyAppTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data.decode(), "<p>Hello, World!</p>")
 
-
     def test_get_customers(self):
         response = self.app.get("/customers")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("Aivanne" in response.data.decode())
 
+    def test_get_customers_by_id(self):
+        response = self.app.get("/customers/1")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Aivanne" in response.data.decode())
+
+
 if __name__ == "__main__":
     unittest.main()
-     
-
-
-
